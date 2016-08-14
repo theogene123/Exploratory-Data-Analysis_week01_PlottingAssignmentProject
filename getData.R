6f647334c39ada702f25262439d2b639b94b55a4
Patrick Merlot <patrick.merlot@gmail.com>
2016-08-14 23:09:02 +0200

    cleaning

--- getData.R
+++ getData.R
@@ -1,12 +1,13 @@
 ### Peer Graded Assignment: Exploratory Data Analysis Course Project (week 1)
-### https://github.com/merlotpa/Exploratory-Data-AnalysisExData_week01_PlottingAssignmentProject
-
-
+### https://github.com/merlotpa/Exploratory-Data-Analysis_week01_PlottingAssignmentProject
 
 
 getData <- function() {
+        library(data.table)
+        library(dplyr) ## (data.table + dplyr code now lives in dtplyr.)
+
         originalDir <- getwd()
-        setwd("/home/patechoc/Documents/COURSE/MOOCs/COURSERA_Datascienc-with-R/course04_Exploratory_Analysis/CODE/Exploratory-Data-Analysis_week01_PlottingAssignmentProject")
+
         ### DOWNLOAD THE DATASET (if not there already)
         mainDir <- getwd()
         rawDir <- "data"
@@ -33,26 +34,27 @@ getData <- function() {
         # $ wc -l household_power_consumption.txt
         # 2075260 household_power_consumption.txt
         
-        # ### 2,075,259 rows and 9 columns
-        # m <- matrix(1,nrow=2075259,ncol=9)
-        # m <- as.data.frame(m)
-        # # m[,1:20] <- sapply(m[,1:20],as.character)
-        # # m[,29:30] <- sapply(m[,29:30],as.factor)
-        # m[,1] <- sapply(m[,1],as.character.Date)
-        # m[,2:9] <- sapply(m[,1],as.factor)
-        # object.size(m)
-        # # 120017224 bytes
-        # print(object.size(m),units="Mb")
-        # # 142.5 Mb
+        ## ### 2,075,259 rows and 9 columns
+        ## m <- matrix(1,nrow=2075259,ncol=9)
+        ## m <- as.data.frame(m)
+        ## # m[,1:20] <- sapply(m[,1:20],as.character)
+        ## # m[,29:30] <- sapply(m[,29:30],as.factor)
+        ## m[,1] <- sapply(m[,1],as.character.Date)
+        ## m[,2:9] <- sapply(m[,1],as.factor)
+        ## object.size(m)
+        ## # 120017224 bytes
+        ## print(object.size(m),units="Mb")
+        ## # 142.5 Mb
         
         
         ### SUBSET THE LARGE DATAFILE
         setwd(dataDir)
-        #### Avoid loading the whole file
+        #### Avoid loading large files and then filter on it if you don't need all the data
         # data <- read.table(paste0(dataDir, "household_power_consumption.txt", collapse = NULL), sep=";")
         # dataToUse <- data[data$Date >= "01/02/2007" & data$Date <= "02/02/2007", ]
         # subset(data, Date >= "01/02/2007" & Date <= "02/02/2007")
-        #### prefer shell commands like this one:
+        
+        #### prefer running shell commands like this one:
         ####    head -n1 household_power_consumption.txt  > subset_household_power_consumption.txt 
         ####    egrep -i "(^1/2/2007)|(^2/2/2007)" household_power_consumption.txt  >> subset_household_power_consumption.txt
         commands <- list("head -n1 household_power_consumption.txt  > subset_household_power_consumption.txt ",
@@ -66,8 +68,6 @@ getData <- function() {
         
         ## a faster way to load the whole file than using read-table() 
         ## with fread from "data.table" package (useful for large data files)
-        library(data.table)
-        library(dplyr) ## (data.table + dplyr code now lives in dtplyr.)
         df <- fread(paste0(dataDir, "/", "subset_household_power_consumption.txt", collapse = NULL),
                     na.strings="?",stringsAsFactors = FALSE)
         
